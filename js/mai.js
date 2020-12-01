70796b5694266b9f48112cbb6f7ddd1935fe398c
milan978gh <amilandev78@gmail.com>
2020-12-01 19:21:34 +0100

    update

---
+++ js/mai.js
@@ -0,0 +1,22 @@
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const vidContainer = document.querySelector('.video-container');
+const switchBtn = document.querySelector('.switch-btn');
+const preloader = document.querySelector('.preloader');
+
+
+window.addEventListener('load', function () {
+  preloader.classList.add('hide-preloader');
+});
+
+switchBtn.addEventListener('click', function () {
+  if (!switchBtn.classList.contains('slide')) {
+    switchBtn.classList.add('slide');
+    vidContainer.pause();
+  } else {
+    switchBtn.classList.remove('slide');
+    vidContainer.play();
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
-
 const vidContainer = document.querySelector('.video-container');
 const switchBtn = document.querySelector('.switch-btn');
-const preloader = document.querySelector('.preloader');
+const playBtn = document.querySelector('.play-btn');
 
-
-window.addEventListener('load', function () {
-  preloader.classList.add('hide-preloader');
-});
 
 switchBtn.addEventListener('click', function () {
-  if (!switchBtn.classList.contains('slide')) {
-    switchBtn.classList.add('slide');
-    vidContainer.pause();
-  } else {
-    switchBtn.classList.remove('slide');
+  if (!switchBtn.classList.contains('play')) {
     vidContainer.play();
+    playBtn.textContent = 'pause';
+    switchBtn.classList.add('play');
+  } else {
+    vidContainer.pause();
+    playBtn.textContent = 'play';
+    switchBtn.classList.remove('play');
   }
+  
 });
